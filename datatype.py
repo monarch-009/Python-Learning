3ab0636c8ce012a67a126925b7b6cbb8d06604c6
monarch-009 <adityasinghmoni@gmail.com>
2023-11-18 14:49:36 +0530

    day 2 python

---
+++ datatype.py
@@ -0,0 +1,25 @@
+print(type("a"))  # <class 'str'>
+
+print(type(1))    # <class 'int'>
+
+print(type(1.1))  # <class 'float'> 
+
+print(type(True)) # <class 'bool'>
+
+print(type([1,2,3]))    # <class 'list'>
+
+print(type((1,2,3)))    # <class 'tuple'>
+
+print(type({1,2,3}))    # <class 'set'>
+
+print(type({1:"a", 2:"b", 3:"c"}))   # <class 'dict'>
+
+print(type(None))       # <class 'NoneType'>   
+
+print(type(1) == int)   # True
+
+print(type(1) == str)   # False
+
+print(type(1) == float) # False
+
+print(type(1) == bool)  # False
